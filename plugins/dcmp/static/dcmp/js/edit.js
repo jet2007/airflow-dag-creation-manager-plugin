7aa67041eb77acf3b16732820276734d81f8c783
caihaimin <caihm@chinatelecom.cn>
2018-01-26 13:45:20 +0800

    x

--- plugins/dcmp/static/dcmp/js/edit.js
+++ plugins/dcmp/static/dcmp/js/edit.js
@@ -222,6 +222,7 @@ Date.prototype.Format = function (fmt) { //author: meizz
                 task_to_element_by_field(lastest_task_id, task, "priority_weight", "number", null, null, "Priority weight of this task against other task. This allows the executor to trigger higher priority tasks before others when things get backed up."),
                 task_to_element_by_field(lastest_task_id, task, "retries", "number", null, null, "The number of retries that should be performed before failing the task."),
                 task_to_element_by_field(lastest_task_id, task, "retry_delay_minutes", "number", null, null, "Delay minutes between retries."),
+                task_to_element_by_field(lastest_task_id, task, "task_more_para", "number", null, null, "task_more_para."),
                 '        </div>',
                 '    </div>',
                 '</div>',
